edf8598366e0a7b619f344d4fe220084960d835a
SinceL <a530478271@gmail.com>
2022-11-14 01:04:16 -0800

    update

--- build_image.sh
+++ build_image.sh
@@ -2,6 +2,18 @@
 
 python_version=3.11
 java_version=17
-
-name='test'
-docker build -t ${name} --build-arg "PYTHON_VERSION=${python_version}" --build-arg "JAVA_VERSION=${java_version}" .
+arg_list=( python_version java_version mirai_git ariadne_git git_pat )
+for arg_name in ${arg_list[@]}; do
+    if [ -z "${!arg_name}" ]; then
+        read -p "Enter $arg_name: " value
+        $arg_name="$value"
+    fi
+done
+image_name='sirius'
+docker build -t ${image_name} \
+    --build-arg "PYTHON_VERSION=${python_version}" \
+    --build-arg "JAVA_VERSION=${java_version}" \
+    --build-arg "MIRAI_GIT=${mirai_git}" \
+    --build-arg "ARIADNE_GIT=${ariadne_git}" \
+    --build-arg "GIT_PAT=${git_pat}" \
+    .
