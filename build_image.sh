81916ca5ec83be638e676b649d21e215b41eef35
SinceL <a530478271@gmail.com>
2022-11-14 03:11:10 -0800

    Update build_image.sh

--- build_image.sh
+++ build_image.sh
@@ -7,5 +7,5 @@ image_name=bot
 docker build -t ${image_name} \
     --build-arg "PYTHON_VERSION=${python_version}" \
     --build-arg "JAVA_VERSION=${java_version}" \
-    --build-arg "USERNAME=${USER}"
+    --build-arg "USERNAME=${USER}" \
     .
